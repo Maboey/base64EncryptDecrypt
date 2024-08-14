dd2c799eb1e36d01f8a9ebbdcbcfe6543731880f
maboey <manuel.martinbalmori@gmail.com>
2024-08-14 11:27:56 +0200

    Initial Commit

---
+++ base64EncryptDecrypt/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  base64EncryptDecrypt
+//
+//  Created by Manuel Martin on 14.08.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
